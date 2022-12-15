f826be6a3fe2b54433e6d32dee929cb87043df5b
PetersonE1 <PetersonE_1@live.com>
2022-12-15 16:39:40 -0500

    whitespace removal (I'm going insane)

--- ULTRAKIT/Main.cs
+++ ULTRAKIT/Main.cs
@@ -15,8 +15,6 @@ namespace ULTRAKIT
     [UKPlugin("petersone1.ultrakitreloaded", "Ultrakit Reloaded", "1.4.0", "A library for weapon loading and common functions", false, false)]
     public class Plugin : UKMod
     {
-
-
         public override void OnModLoaded()
         {
             Loader.Initializer.Initialize();
